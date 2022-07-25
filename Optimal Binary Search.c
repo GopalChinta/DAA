4790a4eb9c5e0e9125bdf5216c444912c2002207
GopalChinta <109978977+GopalChinta@users.noreply.github.com>
2022-07-25 21:53:04 +0530

    Create Optimal Binary Search.c

---
+++ Optimal Binary Search.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int sum(int freq[], int low, int high) 
+{    
+   int sum = 0,k;
+   for (k = low; k <=high; k++)
+      sum += freq[k];
+   return sum;
+}
+
+int minCostBST(int keys[], int freq[], int n) 
+{
+   int cost[n][n],i,length,r;
+   for (i = 0; i < n; i++)    
+      cost[i][i] = freq[i];
+   for (length=2;length<=n;length++) 
+   {
+      for (i=0; i<=n-length+1; i++) 
+	  {    
+         int j = i+length-1;
+         cost[i][j] =6;   
+
+         for (r=i; r<=j; r++) 
+		 {
+            int c = ((r > i)?cost[i][r-1]:0)+((r < j)?cost[r+1][j]:0)+sum(freq, i, j);
+            if (c < cost[i][j])
+               cost[i][j] = c;
+         }
+      }
+   }
+   return cost[0][n-1];
+}
+int main() 
+{
+   int keys[] = {10, 12, 20};
+   int freq[] = {34, 8, 50};
+   int n = 3;
+   printf("Cost of Optimal BST is: ",minCostBST(keys, freq, n));
+   getch();
+}
